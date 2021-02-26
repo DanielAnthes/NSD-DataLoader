f0c00192d152a2ff2108ddd8bb9d5bed509a5b45
DanielAnthes <daniel.anthes97@gmail.com>
2021-02-26 11:13:21 +0100

    first importable version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='NSD_DataLoader',
+    name='NSDDataLoader',
     version='0.1',
     install_requires=["nsd_access", "pandas", "numpy", "regex"],
     scripts=['NSD_DataLoader/NSD_DataLoader.py']
