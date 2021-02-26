0218ec4216864b2e697ff305436792ee0fb3872b
DanielAnthes <daniel.anthes97@gmail.com>
2021-02-26 10:52:41 +0100

    rename to valid package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='NSD-DataLoader',
+    name='NSD_DataLoader',
     version='0.1',
     install_requires=["nsd_access", "pandas", "numpy", "regex"],
     scripts=['NSD_DataLoader/NSD_DataLoader.py']
